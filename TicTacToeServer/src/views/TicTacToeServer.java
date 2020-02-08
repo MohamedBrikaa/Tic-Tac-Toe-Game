1c3aca9ee56965346a526ee7e1d8462b30ce1776
Mohamed Brikaa <MohamedBrikaa@users.noreply.github.com>
2020-02-08 19:48:02 +0200

    update view folder

--- TicTacToeServer/src/views/TicTacToeServer.java
+++ TicTacToeServer/src/views/TicTacToeServer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tictactoeserver;
+package views;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
