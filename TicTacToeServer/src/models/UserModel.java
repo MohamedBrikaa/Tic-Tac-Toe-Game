243a74f99fa6c41c6eb697a80b0277686da8994c
Nour Fayed <nourfayedd@gmail.com>
2020-02-13 20:04:15 +0200

    handling exceptions

--- TicTacToeServer/src/models/UserModel.java
+++ TicTacToeServer/src/models/UserModel.java
@@ -8,6 +8,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /** @author nourfayed */
 
@@ -17,126 +19,173 @@ public class UserModel {
    static final String DB_USER = "root";
    static final String DB_PASSWD = "";
 
-    public static Connection connect() throws SQLException{
+    public static Connection connect() throws SQLException {
        return (Connection) DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWD);
    }
 
    
-   public static boolean addPlayer(User user) throws SQLException{
-       Connection connection = connect();
-  
-       PreparedStatement preparedStatement=connection.prepareStatement("INSERT INTO users ( User_Name, Email,Password,Points,User_Pic,State) VALUES (?,?,?,?,?,?)");
-       
-       preparedStatement.setString(1, user.userName);
-       preparedStatement.setString(2, user.email);
-       preparedStatement.setString(3, user.password);
-       preparedStatement.setInt(4, user.score);
-       preparedStatement.setString(5,"");//hna lsa 7owar l soora
-       preparedStatement.setString(6,user.state);
-       
-       int res=preparedStatement.executeUpdate(); 
-     return res>0;  
+   public static boolean addPlayer(User user) {
+       try {
+           Connection connection = connect();
+           
+           PreparedStatement preparedStatement=connection.prepareStatement("INSERT INTO users ( User_Name, Email,Password,Points,User_Pic,State) VALUES (?,?,?,?,?,?)");
+           
+           preparedStatement.setString(1, user.userName);
+           preparedStatement.setString(2, user.email);
+           preparedStatement.setString(3, user.password);
+           preparedStatement.setInt(4, user.score);
+           preparedStatement.setString(5,"");//hna lsa 7owar l soora
+           preparedStatement.setString(6,user.state);
+           
+           int res=preparedStatement.executeUpdate(); 
+           preparedStatement.close();
+           connection.close();
+           return res>0;
+       } catch (SQLException ex) {
+           Logger.getLogger(UserModel.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       return false;
    }
-   public static boolean validatePlayer(String username, String password) throws SQLException{
-       Connection connection = connect();
-       Statement statement = (Statement) connection.createStatement();
-       ResultSet resultSet=statement.executeQuery("SELECT * FROM users");
-        
-      boolean playerNameFound=false,correctPassword=true;
-    
-      while(resultSet.next()){
-         
-            if(resultSet.getString("User_Name").equals(username)){
-                playerNameFound=true;
-                if(resultSet.getString("Password").equals(password))
-                {
-                    correctPassword=true;
-                }
-                break;
-            }
-         }
-      
+   public static boolean validatePlayer(String username, String password) {
+       try {
+           Connection connection = connect();
+           Statement statement = (Statement) connection.createStatement();
+           ResultSet resultSet=statement.executeQuery("SELECT * FROM users");
+           boolean playerNameFound=false,correctPassword=true;
+           while(resultSet.next()){
+               
+               if(resultSet.getString("User_Name").equals(username)){
+                   playerNameFound=true;
+                   if(resultSet.getString("Password").equals(password))
+                   {
+                       correctPassword=true;
+                   }
+                   break;
+               }
+             
+           } 
+           resultSet.close();
+           connection.close();
+           return correctPassword & playerNameFound;
+       } catch (SQLException ex) {
+           Logger.getLogger(UserModel.class.getName()).log(Level.SEVERE, null, ex);
+       }
+       return false;
+   }
+  
+   public static User playerInfo(String username) {
+       try {
+           Connection connection = connect();
+           Statement statement = (Statement) connection.createStatement();
+           ResultSet resultSet=statement.executeQuery("SELECT * FROM users");
+           
+           
+           while(resultSet.next()){
+               
+               if(resultSet.getString("User_Name").equals(username)){
+                   User user=new User(resultSet.getInt("User_ID"),resultSet.getString("User_Name"),resultSet.getString("Email"),resultSet.getString("Password"),resultSet.getInt("Points"),resultSet.getString("User_Pic"), resultSet.getString("State"));
+                   resultSet.close();
+                   connection.close();
+                   return user;
+               }
+           }
+          
+       } catch (SQLException ex) {
+           Logger.getLogger(UserModel.class.getName()).log(Level.SEVERE, null, ex);
+       }
+        return null;
+   }
    
-      return correctPassword & playerNameFound;
-   }
-  
-   public static User playerInfo(String username) throws SQLException{
-       Connection connection = connect();
-       Statement statement = (Statement) connection.createStatement();
-       ResultSet resultSet=statement.executeQuery("SELECT * FROM users");
-        
-    
-      while(resultSet.next()){
-         
-            if(resultSet.getString("User_Name").equals(username)){
-                return new User(resultSet.getInt("User_ID"),resultSet.getString("User_Name"),resultSet.getString("Email"),resultSet.getString("Password"),resultSet.getInt("Points"),resultSet.getString("User_Pic"), resultSet.getString("State"));
-            }
-         }
+  public static Vector<User> returnAllPlayers() {
+       try {
+           Vector<User>tmp=new Vector<User>();
+           
+           Connection connection = connect();
+           Statement statement = (Statement) connection.createStatement();
+           ResultSet resultSet=statement.executeQuery("SELECT * FROM users");
+           while(resultSet.next()){
+               
+               User currentPlayer=new User(resultSet.getInt("User_ID"),resultSet.getString("User_Name"),resultSet.getString("Email"),resultSet.getString("Password"),resultSet.getInt("Points"),resultSet.getString("User_Pic"), resultSet.getString("State"));
+               tmp.add(currentPlayer);
+           }  
+           resultSet.close();
+           connection.close();
+           return tmp;
+       } catch (SQLException ex) {
+           Logger.getLogger(UserModel.class.getName()).log(Level.SEVERE, null, ex);
+       }
        return null;
-   }
-   
-  public static Vector<User> returnAllPlayers() throws SQLException{
-      Vector<User>tmp=new Vector<User>();
-      Connection connection = connect();
-      Statement statement = (Statement) connection.createStatement();
-      ResultSet resultSet=statement.executeQuery("SELECT * FROM users");
-      while(resultSet.next()){
-         
-           User currentPlayer=new User(resultSet.getInt("User_ID"),resultSet.getString("User_Name"),resultSet.getString("Email"),resultSet.getString("Password"),resultSet.getInt("Points"),resultSet.getString("User_Pic"), resultSet.getString("State"));
-           tmp.add(currentPlayer);
-         }
-      return tmp;  
   }
    
-  public static boolean updatePlayerScore(String username, int score) throws SQLException{
-      Connection connection = connect();
-      
-      Statement statement = (Statement) connection.createStatement();
-      ResultSet resultSet=statement.executeQuery("SELECT * FROM users");
-      int lastScore=0;
-      while(resultSet.next()){
-         
-           if(resultSet.getString("User_name")== username){
-               lastScore=resultSet.getInt("Points");
-               break;
+  public static boolean updatePlayerScore(String username, int score) {
+       try {
+           Connection connection = connect();
+           
+           Statement statement = (Statement) connection.createStatement();
+           ResultSet resultSet=statement.executeQuery("SELECT * FROM users");
+           int lastScore=0;
+           while(resultSet.next()){
+               
+               if(resultSet.getString("User_name")== username){
+                   lastScore=resultSet.getInt("Points");
+                   break;
+               }
            }
-         }
-      
-      score+=lastScore;
-      
-      PreparedStatement preparedStatement=connection.prepareStatement("UPDATE users SET Points =? where User_name=? ");
-    
-      preparedStatement.setInt(1, score);
-      preparedStatement.setString(2, username);
-      int res=preparedStatement.executeUpdate(); 
-     return res>0;  
-      
+           
+           score+=lastScore;
+           
+           PreparedStatement preparedStatement=connection.prepareStatement("UPDATE users SET Points = ? where User_name=? ");
+           
+           preparedStatement.setInt(1, score);
+           preparedStatement.setString(2, username);
+           int res=preparedStatement.executeUpdate();
+           
+           resultSet.close();
+           preparedStatement.close();
+           connection.close();
+           return res>0;
+       } catch (SQLException ex) {  
+           Logger.getLogger(UserModel.class.getName()).log(Level.SEVERE, null, ex);
+       }
+      return false;
    }
    
-   public static boolean updatePlayerState(String username,String state) throws SQLException{
-      Connection connection = connect();
-      PreparedStatement preparedStatement=connection.prepareStatement("UPDATE users SET State =? where User_name=? ");
-    
-      preparedStatement.setString(1, state);
-      preparedStatement.setString(2, username);
-        int res=preparedStatement.executeUpdate(); 
-     return res>0;  
-      
+   public static boolean updatePlayerState(String username,String state) {
+       try {
+           Connection connection = connect();
+           PreparedStatement preparedStatement=connection.prepareStatement("UPDATE users SET State =? where User_name=? ");
+           
+           preparedStatement.setString(1, state);
+           preparedStatement.setString(2, username);
+           int res=preparedStatement.executeUpdate();  
+           preparedStatement.close();
+           connection.close();
+           return res>0;
+           
+       } catch (SQLException ex) {
+           Logger.getLogger(UserModel.class.getName()).log(Level.SEVERE, null, ex);
+       }
+      return false;
    }
    
-   public static void main(String[] args) throws SQLException{
+   public static void main(String[] args) {
    
        Vector<User>v=new Vector<>();
        v=returnAllPlayers();
        for(int i=0;i<v.size();i++){
            System.out.println(v.get(i).userName);
+           System.out.println(v.get(i).score);
        }
        boolean b=UserModel.updatePlayerState("nour","ONLINE");
-       System.out.println(b);
+     //  System.out.println(b);
        
        boolean b2=UserModel.updatePlayerScore("nour", 3);
         System.out.println(b2);
-    
+        
+      for(int i=0;i<v.size();i++){
+           System.out.println(v.get(i).userName);
+           System.out.println(v.get(i).score);
+       }
    }
    
  
