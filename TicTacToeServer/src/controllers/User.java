e4ccd5ec8b5bac050bf09b920ed06977a59fdc66
Nour Fayed <nourfayedd@gmail.com>
2020-02-13 16:36:56 +0200

    adding constructor

--- TicTacToeServer/src/controllers/User.java
+++ TicTacToeServer/src/controllers/User.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controllers;
 
 /**
@@ -10,9 +6,26 @@ package controllers;
  * @author Mohamed-Suliman
  */
 public class User {
-    int userID;
-    String userName;
-    String Email;
-    String State;
-    String password;
+    public int userID;
+    public String userName;
+    public String email;
+    public String state;
+    public String password;
+    public String pic;
+    public int score;
+    
+    public User(int userId,String userName,String email,String password,int score ,String pic,String state){
+        this.userID=userId;
+        this.userName=userName;
+        this.email=email;
+        this.state=state;
+        this.password=password;
+        this.score=score;
+        this.pic=pic;
+        
+        
+        
+        
+    }
+    
 }
