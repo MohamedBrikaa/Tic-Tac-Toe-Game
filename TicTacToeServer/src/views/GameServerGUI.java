345ec5edae5f5ab96578bc59d794dd73c583d577
Mohamed Brikaa <MohamedBrikaa@users.noreply.github.com>
2020-02-14 15:24:00 +0200

    update game server GUI

--- TicTacToeServer/src/views/GameServerGUI.java
+++ TicTacToeServer/src/views/GameServerGUI.java
@@ -11,6 +11,7 @@ import java.io.*;
 import java.net.*;
 import java.util.Vector;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.*;
