805de104ea9434ce66a27941b84f04387c4c6ee6
Nour Fayed <nourfayedd@gmail.com>
2020-02-16 10:14:50 +0200

    added initial game server.

--- TicTacToeServer/src/controllers/GameMatch.java
+++ TicTacToeServer/src/controllers/GameMatch.java
@@ -1,29 +1,102 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controllers;
 
-import models.*;
-/**
- *
- * @author Mohamed-Suliman
- */
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class GameMatch extends Thread{
-    User player1;
-    User player2;
-
-    public void initGame(String user1Name, String user2Name) {
-        player1 = UserModel.playerInfo(user1Name);
-        player2 = UserModel.playerInfo(user2Name);
+   
+    String u1,u2;
+    Socket s1,s2;
+    DataInputStream[] dis;
+    PrintStream[] ps;
+    char[][] grid=new char[3][3];
+    char[] XO=new char[]{'X','O'};
+   
+    public GameMatch(String u1,String u2,Socket s1,Socket s2){
+        try {
+            this.u1=u1;
+            this.u2=u2;
+            this.s1=s1;
+            this.s2=s2;
+            
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    grid[i][j]='-';
+                }
+            }
+            
+           
+            
+            //ba3dein han-assign kol wa7d fihom x aw o
+            dis[0]=new DataInputStream(s1.getInputStream());
+            ps[0]=new PrintStream(s1.getOutputStream());
+            ps[0].println(XO[0]);
+            
+            dis[1]=new DataInputStream(s2.getInputStream());
+            ps[1]=new PrintStream(s2.getOutputStream());
+            ps[1].println(XO[1]);
+            start();
+            
+        } catch (IOException ex) {
+            Logger.getLogger(GameMatch.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
-
-    public void stopGame() {
-
+    
+    public void playerTurn(int playerNumber){
+        try {
+            //ba3dein nbda2 l game ba2a kol wa7d 3aleh l dor ha2olo 3aleik l dor 
+            ps[playerNumber].println("It is your turn");
+           
+            //ba3dein kol wa7d yb3at 7aga 
+            String index=dis[playerNumber].readLine();
+            int x=Integer.parseInt(index);
+            int i=x%3;
+            int j=x/3;
+            
+            //ha-update l grid w ab3atha lel etnein
+            grid[i][j]=XO[playerNumber];
+            ps[0].println(index);
+            ps[1].println(index);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(GameMatch.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
     }
-
-    public void resumeGame() {
-
+    public void run(){
+        int playerNumber=0;
+        int cntr=0;
+        while(cntr<9){
+            playerTurn(playerNumber);
+            playerNumber++;
+            playerNumber%=2;  
+            cntr++;
+        }
+        
     }
+    
+    
+    public void stopGame(){
+    
+    }
+    
+    public void resumeGame(){
+    
+    }
+    
+    public static void main(String[] args){
+    
+    
+    }
+    
 }
