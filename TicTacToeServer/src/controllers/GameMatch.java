5b13ecc2edb5e0071f8989ad6f394c5c612dbc27
Nour Fayed <nourfayedd@gmail.com>
2020-02-18 23:05:07 +0200

    added pause function and uploading paused matches in the beginning of the game

--- TicTacToeServer/src/controllers/GameMatch.java
+++ TicTacToeServer/src/controllers/GameMatch.java
@@ -10,11 +10,15 @@ import java.util.Arrays;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import models.UserModel;
+import recordmatch.RecordMatch;
+import recordmatch.Match;
 
 public class GameMatch extends Thread{
    
     String u1,u2;
     Socket s1,s2;
+    int player1Id,player2Id;
     DataInputStream[] dis= new DataInputStream[2];
     PrintStream[] ps= new PrintStream[2];
     String[][] grid=new String[3][3];
@@ -27,15 +31,33 @@ public class GameMatch extends Thread{
             this.s1=s1;
             this.s2=s2;
             
-            for(int i=0;i<3;i++){
-                for(int j=0;j<3;j++){
-                    grid[i][j]="-";
+            player1Id=UserModel.playerId(u1);
+            player2Id=UserModel.playerId(u2);
+            //first check if there is a saved game???
+            
+            Match savedMatch=RecordMatch.getRecordedMatch(player1Id,player2Id);
+            
+            if(savedMatch== null){
+                System.out.println("A new Match");
+                for(int i=0;i<3;i++){
+                    for(int j=0;j<3;j++){
+                        grid[i][j]="-";
+                    }
                 }
             }
+            else {
+                System.out.println("A saved Match");
+                for(int i=0;i<3;i++){
+                    for(int j=0;j<3;j++){
+                        grid[i][j]=savedMatch.grid[i][j];
+                    }
+                }
+            }
+            
             System.out.println("this is s1 "+this.s1);
             System.out.println("this is s2 "+this.s2);
             
-            
+            //initialize dis and ps and assign X or O to each player
             dis[0]=new DataInputStream(this.s1.getInputStream());
             ps[0]=new PrintStream(this.s1.getOutputStream());
             ps[0].println(XO[0]);
@@ -53,7 +75,7 @@ public class GameMatch extends Thread{
     
     public void playerTurn(int playerNumber){
         try {
-            
+            //Assign turns to each player 
             ps[playerNumber].println("It is your turn");
             ps[(playerNumber+1)%2].println("It is not your turn.");
            
@@ -61,18 +83,26 @@ public class GameMatch extends Thread{
             String index="-";
          
             System.out.println(index); 
+             //read chosen index or pause game
             index=dis[playerNumber].readLine();
-          
-            System.out.println(index);
-            
-            int x=Integer.parseInt(index);
-            int i=x%3;
-            int j=x/3;
             
             
-            grid[i][j]=XO[playerNumber];
-            ps[0].println(index);
-            ps[1].println(index);
+            if(index.equals("pause")){
+                System.out.println(index);
+               // pauseGame(playerNumber);
+            }
+            else{
+                System.out.println(index);
+
+                int x=Integer.parseInt(index);
+                int i=x%3;
+                int j=x/3;
+
+                //update grid and send new update to both players
+                grid[i][j]=XO[playerNumber];
+                ps[0].println(index);
+                ps[1].println(index);
+            }
             
         } catch (IOException ex) {
             Logger.getLogger(GameMatch.class.getName()).log(Level.SEVERE, null, ex);
@@ -92,13 +122,27 @@ public class GameMatch extends Thread{
         }
         
     }
+   
+  
     
-    
-    public void stopGame(){
-    
-    }
-    
-    public void resumeGame(){
+    public void pauseGame(int playerNumber){
+         
+        //insert match to database
+        boolean b=RecordMatch.addMatch(player1Id,player2Id,grid,playerNumber);
+        
+        //close everything 
+       try {
+            for(int i=0;i<2;i++){
+                    dis[i].close();
+                    ps[i].close();          
+               }
+            s1.close();
+            s2.close();
+        } catch (IOException ex) {
+                Logger.getLogger(GameMatch.class.getName()).log(Level.SEVERE, null, ex);
+            }
+       //how to close the thread???
+       
     
     }
     
