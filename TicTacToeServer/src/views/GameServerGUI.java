13b2e7e1c08b766be3fbed0b6194d909531bd50d
Mohamed Brikaa <MohamedBrikaa@users.noreply.github.com>
2020-02-22 21:57:15 +0200

    update server gui

--- TicTacToeServer/src/views/GameServerGUI.java
+++ TicTacToeServer/src/views/GameServerGUI.java
@@ -20,6 +20,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
